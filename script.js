d2af233fb44c708864c1f0152dc49d7c67e5d17d
unknown <ddiazmo@unal.edu.co>
2022-05-13 14:45:13 -0500

    Se agrega 2da gráfica sobre tráfico de zonas wifi

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var urlMetricasSitio = "https://www.datos.gov.co/resource/htta-bj9v.json"
+var urlMetricasSitio2 = "https://www.datos.gov.co/resource/vbcy-hvi2.json"
 
 var visitas = [];
 var fecha = [];
+var altitud = [];
+var fuente = [];
 
 fetch(urlMetricasSitio)
 .then(respuestaMetricasSitio => respuestaMetricasSitio.json())
@@ -47,3 +50,43 @@ fetch(urlMetricasSitio)
     Plotly.newPlot("indiceInfoClas", grafMetricasSitio, layoutMetricasSitio);
 
 });
+
+fetch(urlMetricasSitio2)
+.then(respuestaMetricasSitio2 => respuestaMetricasSitio2.json())
+.then(function transformar (respuestaMetricasSitio2) {
+
+    console.log(respuestaMetricasSitio2);
+
+    respuestaMetricasSitio2.forEach(function agregar (respuestaMetricasSitio2) 
+    {
+        //Si los datos son diferentes de vacío
+        if (respuestaMetricasSitio2.municipio != undefined && respuestaMetricasSitio2.tiempo_de_sesion != undefined) 
+        {
+            altitud.push(respuestaMetricasSitio2.municipio);
+            fuente.push(respuestaMetricasSitio2.tiempo_de_sesion);
+        }
+    });
+
+var trace1 = {
+    y: fuente,
+    mode: 'markers',
+    marker: {
+      size: 10,
+      //color: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43,44,45]
+      color: 'rgba(219, 64, 82, 0.6)',
+      
+    }
+  };
+  
+  var data = [trace1];
+  
+  var layout = {
+    title: 'Información del tráfico en zonas Wi-Fi en el Valle del Cauca'
+  };
+  
+  Plotly.newPlot('myDiv', data, layout);
+  
+  
+});
+
+
